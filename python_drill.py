dfd3cf4eb30446a22db7c4998f7dc2b6e1b20a94
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-14 22:12:29 -0800

    corrected python drill
    re-submitting corrected python drill

--- python_drill.py
+++ python_drill.py
@@ -1,51 +1,46 @@
 
 import os
+
 import sys
 
 path = 'C:\\Users\\Camille\\python_drill'
 dirs = os.listdir(path)
+# print(dirs)
 
 for file in dirs:
-    print(file)
+    
+    if file.endswith(".txt"):
+        m = os.path.getmtime(os.path.join(path,file))
+        print(file,' ',m)
 
-result = os.path.join('C:\\Users\\Camille\\python_drill','desktop.ini')
-print(result)
+##result = os.path.join('C:\\Users\\Camille\\python_drill','desktop.ini')
+##
+##
+##result = os.path.join('C:\\Users\\Camille\\python_drill','JayO.txt')
+##print(result)
+##m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','JayO.txt'))
+##print(m)
+##
+##result = os.path.join('C:\\Users\\Camille\\python_drill','practice.html')
+##result = os.path.join('C:\\Users\\Camille\\python_drill','Practice1.html')
+##result = os.path.join('C:\\Users\\Camille\\python_drill','practice2.html')
+##result = os.path.join('C:\\Users\\Camille\\python_drill','practice2.html')
+##
+##
+##result = os.path.join('C:\\Users\\Camille\\python_drill','sql_essay.txt')
+##print(result)
+##m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','sql_essay.txt'))
+##print(m)
+##
+##result = os.path.join('C:\\Users\\Camille\\python_drill','test.txt')
+##print(result)
+##m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','test.txt'))
+##print(m)
+##
+##result = os.path.join('C:\\Users\\Camille\\python_drill','testCode3.py')
+##result = os.path.join('C:\\Users\\Camille\\python_drill','testCode4.py')
+##result = os.path.join('C:\\Users\\Camille\\python_drill','testCode5.py')
 
-result = os.path.join('C:\\Users\\Camille\\python_drill','JayO.txt')
-print(result)
-m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','JayO.txt'))
-print(m)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','practice.html')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','Practice1.html')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','practice2.html')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','practice2.html')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','sql_essay.txt')
-print(result)
-m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','sql_essay.txt'))
-print(m)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','test.txt')
-print(result)
-m = os.path.getmtime(os.path.join('C:\\Users\\Camille\\python_drill','test.txt'))
-print(m)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','testCode3.py')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','testCode4.py')
-print(result)
-
-result = os.path.join('C:\\Users\\Camille\\python_drill','testCode5.py')
-print(result)
 
 
 
